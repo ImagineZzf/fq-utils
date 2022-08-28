f61c5969d9004fcfba73a68e1a3e9eb23cfb7170
zhangzhanfeng <zzf728393@163.com>
2022-08-28 15:37:44 +0800

    feat: 完善日期格式化

--- src/date.ts
+++ src/date.ts
@@ -1,22 +1,27 @@
+import { reviseLength } from "./common"
+import { dateType } from "./interface"
+
 /**
  * @description: 获取格林威治时间
  * @param {Date} date 传入日期
  * @return {*}
  */
-export const getDate = (date: Date | string | number): Date => {
+export const getDate = (date: dateType): Date => {
   if (date instanceof Date) {
     return date
   }
   if (typeof date === 'number') {
-    // TODO: 如果是number，增加位数不全
-    // TODO: 看看typedoc有没有好看点的主题，看看怎么配置
-    return new Date(date)
+    // 如果是number，增加位数不全
+    return new Date(reviseLength(date, 13))
   }
   if (typeof date === 'string') {
     if (!!date) {
-      // TODO: 如果是number字符串，增加位数不全
+      // 如果是number字符串，增加位数不全
+      if (!isNaN(+date)) {
+        date = reviseLength(date, 13)
+      }
       // eslint-disable-next-line no-useless-escape
-      date = date.replace(/\-/g, '/').replace(/\./g, '/')
+      date = (date as string).replace(/\-/g, '/').replace(/\./g, '/')
       let newDate = new Date(date)
       // 这里判断Invalid Date的清空，做浏览器兼容性处理
       if (isNaN(newDate.getTime())) {
