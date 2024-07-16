21a21612c6b6adb5943d614bcafa84891f9fc458
zhangzhanfeng <zhangzhanfeng@zhangzhanfengdeMacBook-Pro.local>
2024-07-16 14:52:58 +0800

    feat: 增加一维数组和tree之间的处理函数

--- src/array.ts
+++ src/array.ts
@@ -31,7 +31,7 @@ export const getArrayFromNumber = (params: number): number[] => {
  * @param key 唯一code
  * @param parentKey 父节点字段
  * @param childrenKey 子节点字段
- * @returns 
+ * @returns 树状tree数组
  */
 export const getTreeFromArray = (params: any[], key = 'code', parentKey = 'parent', childrenKey = 'children'): any[] => {
   if (!isArray(params, 0)) {
@@ -52,4 +52,64 @@ export const getTreeFromArray = (params: any[], key = 'code', parentKey = 'paren
     }
   })
   return treeData
+}
+
+/**
+ * @group 【array】
+ * @category 将tree数组转换成一维数组
+ * @param params 传入的tree数组
+ * @param childrenKey 子节点字段
+ * @returns 一维数组
+ */
+export const getArrayFromTree = (params: any[], childrenKey = 'children') => {
+  if (!isArray(params, 0)) {
+    return []
+  }
+  const list = []
+  const queue = [...params]
+  while(queue.length) {
+    const node = {...queue.shift()}
+    const children = node[childrenKey]
+    if(children) {
+      queue.push(...children)
+    }
+    delete node[childrenKey]
+    list.push(node)
+  }
+  return list
+}
+
+/**
+ * @group 【array】
+ * @category 从tree数组中过滤需要的数据
+ * @param treeData 
+ * @param predicate 
+ * @param childrenKey 
+ * @returns 过滤后的数据
+ */
+export const filterTree = (treeData: any[], predicate: any = () => true, childrenKey = 'children') => {
+  const result = [] // 最终结果
+  const temp = [] // 临时数据
+
+  function deepFilter(arr: any[]) {
+    for(let i = 0, len = arr.length; i < len; i++) {
+      if (predicate(arr[i])) {
+        if (temp.length) {
+          result.push(...temp)
+        }
+        // 如果符合条件，则直接结束
+        result.push(arr[i])
+        break;
+      }
+      // 不符合条件，则判断是否有children
+      if (arr[i][childrenKey] && Array.isArray(arr[i][childrenKey])) {
+        temp.push(arr[i])
+        deepFilter(arr[i][childrenKey])
+      }
+    }
+  }
+
+  deepFilter(treeData)
+
+  return result
 }
